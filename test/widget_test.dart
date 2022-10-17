3841e1baedf034a1dcb044c1dc150a3a9eb47670
coditas <coditas@11174-lap-0161.local>
2022-10-17 09:30:02 +0530

    Fix my app error in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:news_app_assignment6/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
